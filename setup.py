38f51a4063ab42496edc937417fc8fc3968b62f7
Fenton Travers <fenton.travers@oracle.com>
2013-10-16 13:11:15 +0700

    Releasing: [project]: pscripts, [version]: 0.1.135

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools.command.install  import  install  as  _install
 from setuptools import setup
 import os
-version='0.1.134'
+version='0.1.135'
 name='pscripts'
 scripts = [
     'scripts/python-deployment',
